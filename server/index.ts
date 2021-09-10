8cfe1aac8f683260a7d39850731ac37c40a900a6
Jonah Grimes <jgrimes@appriss.com>
2021-09-10 11:12:57 -0400

    added nexus schema and type generation
    Signed-off-by: Jonah Grimes <jgrimes@appriss.com>

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,24 @@
-import {BookableAPI} from "./bookables/datasource";
-import {BookingAPI} from "./bookings/datasource";
-import {UserAPI} from "./users/datasource";
-import * as BookableTypes from "./bookables/types";
-import * as BookingTypes from "./bookings/types";
-import * as UserTypes from "./users/types";
+import {makeSchema} from "nexus"
+import path from "path"
 
-import {makeSchema} from "nexus";
+import {BookableAPI} from "./bookables/datasource"
+import {BookingAPI} from "./bookings/datasource"
+import {UserAPI} from "./users/datasource"
+import * as BookableTypes from "./bookables/types"
+import * as BookingTypes from "./bookings/types"
+import * as UserTypes from "./users/types"
 
 export const dataSources = () => ({
   bookableAPI: new BookableAPI("http://localhost:3001"),
   bookingAPI: new BookingAPI("http://localhost:3001"),
-  userAPI: new UserAPI("http://localhost:3001"),
-});
+  userAPI: new UserAPI("http://localhost:3001")
+})
 
 export const schema = makeSchema({
   types: [BookableTypes, BookingTypes, UserTypes],
-  shouldGenerateArtifacts: false,
-});
+  shouldGenerateArtifacts: process.env.NODE_ENV === "development",
+  outputs: {
+    typegen: path.join(process.cwd(), "generated", "nexus", "bookings-typegen.ts"),
+    schema: path.join(process.cwd(), "generated", "nexus", "bookings-schema.gql")
+  }
+})
